8e34ab1cf5b972e7844908b63e8d4711a1c091a2
fderue <francois.xavier.derue@gmail.com>
2024-08-21 16:55:30 +0200

    refactor: update navigation bar in project pages

--- js/navbar.js
+++ js/navbar.js
@@ -6,16 +6,10 @@ fetch("../navbar.html")
     // Now that the HTML is loaded, query all <a> elements within the .navbar-nav class
     const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
 
-    // Extract the href attribute from each <a> element
-    const hrefs = Array.from(navLinks).map((link) => link.getAttribute("href"));
-
-    // Log the hrefs to the console
-    console.log(hrefs);
-
     // Highlight the active link based on the current URL
     const currentPath = window.location.pathname;
     navLinks.forEach((link) => {
-      if (link.getAttribute("href") === currentPath.split("/").pop()) {
+      if (currentPath.includes(link.getAttribute("href"))) {
         link.classList.add("active");
       }
     });
