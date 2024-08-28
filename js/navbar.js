a03b9f8230dfe2fdb7c3e2c2b9574ee3baacfab6
fderue <francois.xavier.derue@gmail.com>
2024-08-28 09:37:50 +0200

    use a css trick to keep nav-item highlighted
    It allows to avoid the use of state in javascript

--- js/navbar.js
+++ js/navbar.js
@@ -3,16 +3,6 @@ fetch("../navbar.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navbar-placeholder").innerHTML = data;
-    // Now that the HTML is loaded, query all <a> elements within the .navbar-nav class
-    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
-
-    // Highlight the active link based on the current URL
-    const currentPath = window.location.pathname;
-    navLinks.forEach((link) => {
-      if (currentPath.includes(link.getAttribute("href"))) {
-        link.classList.add("active");
-      }
-    });
 
     // Allow clicking on dropdown parent link
     document
