0f55f2990a5db2d1c30fa618d5574ef77150cffa
fderue <francois.xavier.derue@gmail.com>
2025-06-24 12:30:52 +0200

    refactoring: remove redundant call

--- js/project-animation.js
+++ js/project-animation.js
@@ -1,231 +1,119 @@
 const animation = (p) => {
-  let CANVAS_WIDTH = 100;
-  let CANVAS_HEIGHT = 100;
+  const THUMBNAIL_ID = "project-thumbnail";
+  let canvasWidth = 100;
+  let canvasHeight = 100;
   let rotationAngle = 0;
+  let bgLayer;
 
   p.setup = () => {
-    const thumbnailId = "project-thumbnail";
-    const thumbnail = document.getElementById(thumbnailId);
+    const container = document.getElementById(THUMBNAIL_ID);
+    canvasWidth = container.offsetWidth;
+    canvasHeight = container.offsetHeight;
 
-    CANVAS_WIDTH = thumbnail.offsetWidth;
-    CANVAS_HEIGHT = thumbnail.offsetHeight;
-    const canvas = p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-    canvas.parent(thumbnailId);
+    const canvas = p.createCanvas(canvasWidth, canvasHeight);
+    canvas.parent(THUMBNAIL_ID);
 
-    drawBackground(canvas);
+    bgLayer = p.createGraphics(canvasWidth, canvasHeight);
+    drawStaticBackground(bgLayer);
   };
 
   p.draw = () => {
-    // Draw and move the shape
-    drawBackground({ width: CANVAS_WIDTH, height: CANVAS_HEIGHT });
+    // Draw cached background
+    p.image(bgLayer, 0, 0);
+
+    // Animated gear following mouse
     rotationAngle += 0.8;
-    const innerRadius = CANVAS_WIDTH * 0.015;
+    const innerRadius = canvasWidth * 0.015;
     const outerRadius = innerRadius * 1.4;
     const nbTooth = 8;
-    addGear(
-      p.mouseX,
-      p.mouseY,
-      rotationAngle,
-      innerRadius,
-      outerRadius,
-      nbTooth
-    );
+    addGear(p, p.mouseX, p.mouseY, rotationAngle, innerRadius, outerRadius, nbTooth);
   };
 
   p.windowResized = () => {
-    // Resize canvas if window changes size
-    const container = document.getElementById("project-thumbnail");
-    CANVAS_WIDTH = container.offsetWidth;
-    CANVAS_HEIGHT = container.offsetHeight;
-    p.resizeCanvas(container.offsetWidth, container.offsetHeight);
-    drawBackground({
-      width: container.offsetWidth,
-      height: container.offsetHeight,
-    });
+    const container = document.getElementById(THUMBNAIL_ID);
+    canvasWidth = container.offsetWidth;
+    canvasHeight = container.offsetHeight;
+    p.resizeCanvas(canvasWidth, canvasHeight);
+
+    // Rebuild cached background
+    bgLayer = p.createGraphics(canvasWidth, canvasHeight);
+    drawStaticBackground(bgLayer);
   };
 
-  function drawBackground(canvas) {
-    p.background(40);
-    // Start in the center of the canvas
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
+  function drawStaticBackground(pg) {
+    pg.background(40);
+    const centerX = pg.width / 2;
+    const centerY = pg.height / 2;
 
-    const gearInnerRadius = canvas.width * 0.015;
-    const gearOuterRadius = gearInnerRadius * 1.4;
-    const pitchRadius = (gearInnerRadius + gearOuterRadius) / 2.0;
+    const innerRadius = pg.width * 0.015;
+    const outerRadius = innerRadius * 1.4;
+    const pitchRadius = (innerRadius + outerRadius) / 2;
     const nbTooth = 8;
 
-    // Add static background
-    addGear(0, 0, 0, gearInnerRadius, gearOuterRadius, nbTooth);
+    const positions = [
+      [0, 0, 0],
+      [2 * pitchRadius, pitchRadius, 30],
+      [2 * pitchRadius, 3.1 * pitchRadius, 40],
+      [0, pg.height, 0],
+      [2 * pitchRadius, pg.height - pitchRadius, 15],
+      [3 * pitchRadius, pg.height - 2.9 * pitchRadius, 15],
+      [centerX, 2 * pitchRadius, 0],
+      [centerX - 2 * pitchRadius, 1.2 * pitchRadius, 25],
+      [centerX + 2 * pitchRadius, 1.2 * pitchRadius, 25],
+      [pg.width, 0, 0],
+      [pg.width - 1.5 * pitchRadius, 1.5 * pitchRadius, 30],
+      [pg.width - 3 * pitchRadius, 3 * pitchRadius, 35],
+      [pg.width, pg.height, 0],
+      [pg.width - 1.8 * pitchRadius, pg.height - pitchRadius, 30],
+      [pg.width - 3.8 * pitchRadius, pg.height - 1.5 * pitchRadius, 22],
+    ];
 
-    addGear(
-      2 * pitchRadius,
-      pitchRadius,
-      30,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      2 * pitchRadius,
-      3.1 * pitchRadius,
-      40,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(0, canvas.height, 0, gearInnerRadius, gearOuterRadius, nbTooth);
-
-    addGear(
-      2 * pitchRadius,
-      canvas.height - 1 * pitchRadius,
-      15,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      3 * pitchRadius,
-      canvas.height - 2.9 * pitchRadius,
-      15,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      centerX,
-      2 * pitchRadius,
-      0,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      centerX - 2 * pitchRadius,
-      1.2 * pitchRadius,
-      25,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      centerX + 2 * pitchRadius,
-      1.2 * pitchRadius,
-      25,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(canvas.width, 0, 0, gearInnerRadius, gearOuterRadius, nbTooth);
-
-    addGear(
-      canvas.width - 1.5 * pitchRadius,
-      1.5 * pitchRadius,
-      30,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      canvas.width - 3 * pitchRadius,
-      3 * pitchRadius,
-      35,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      canvas.width,
-      canvas.height,
-      0,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      canvas.width - 1.8 * pitchRadius,
-      canvas.height - pitchRadius,
-      30,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
-    );
-
-    addGear(
-      canvas.width - 3.8 * pitchRadius,
-      canvas.height - 1.5 * pitchRadius,
-      22,
-      gearInnerRadius,
-      gearOuterRadius,
-      nbTooth
+    positions.forEach(([x, y, angle]) =>
+      addGear(pg, x, y, angle, innerRadius, outerRadius, nbTooth)
     );
   }
 
-  function drawGear(innerRadius = 10, outerRadius = 20, nbTooth = 8) {
-    p.noFill();
-    p.stroke(255);
-    p.circle(0, 0, 0.4 * innerRadius * 2);
+  function addGear(pg, x, y, angleDeg, innerRadius, outerRadius, nbTooth) {
+    pg.push();
+    pg.translate(x, y);
+    pg.angleMode(p.DEGREES);
+    pg.rotate(angleDeg);
+    drawGear(pg, innerRadius, outerRadius, nbTooth);
+    pg.pop();
+  }
 
-    const angleRadStep = (2 * Math.PI) / nbTooth;
-    const halfAngleRadStep = angleRadStep / 2.0;
-    const quartAngleRadStep = halfAngleRadStep / 2.0;
+  function drawGear(pg, innerRadius = 10, outerRadius = 20, nbTooth = 8) {
+    pg.noFill();
+    pg.stroke(255);
+    pg.circle(0, 0, 0.4 * innerRadius * 2);
+
+    const angleStep = (2 * Math.PI) / nbTooth;
+    const halfStep = angleStep / 2;
+    const quarterStep = halfStep / 2;
 
     for (let i = 0; i < nbTooth; i++) {
-      const curAngleRad = i * angleRadStep;
+      const curAngle = i * angleStep;
 
-      p.push();
-      p.angleMode(p.RADIANS);
+      pg.push();
+      pg.angleMode(p.RADIANS);
+      pg.rotate(curAngle);
+      pg.beginShape();
 
-      p.rotate(curAngleRad);
-      p.beginShape();
-
-      const x1 = -innerRadius * Math.sin(quartAngleRadStep);
-      const y1 = innerRadius * Math.cos(quartAngleRadStep);
-      p.vertex(x1, y1);
+      const x1 = -innerRadius * Math.sin(quarterStep);
+      const y1 = innerRadius * Math.cos(quarterStep);
+      pg.vertex(x1, y1);
 
       const x2 = 0.75 * x1;
-      const y2 = outerRadius * Math.cos(quartAngleRadStep);
-      p.vertex(x2, y2);
+      const y2 = outerRadius * Math.cos(quarterStep);
+      pg.vertex(x2, y2);
+      pg.vertex(-x2, y2);
+      pg.vertex(-x1, y1);
 
-      const x3 = -x2;
-      const y3 = y2;
-      p.vertex(x3, y3);
+      pg.endShape();
 
-      const x4 = -x1;
-      const y4 = y1;
-      p.vertex(x4, y4);
-      p.endShape();
-
-      p.arc(
-        0,
-        0,
-        2 * innerRadius,
-        2 * innerRadius,
-        quartAngleRadStep,
-        3 * quartAngleRadStep
-      );
-      p.pop();
+      pg.arc(0, 0, 2 * innerRadius, 2 * innerRadius, quarterStep, 3 * quarterStep);
+      pg.pop();
     }
-  }
-
-  function addGear(x, y, angleDeg, gearInnerRadius, gearOuterRadius, nbTooth) {
-    p.push();
-    p.translate(x, y);
-    p.angleMode(p.DEGREES);
-
-    p.rotate(angleDeg);
-    drawGear(gearInnerRadius, gearOuterRadius, nbTooth);
-    p.pop();
   }
 };
 
